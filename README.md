5289dbd0da30783754b74dff596ac93aa1bba314
biggsbenjamin <bb2515@ic.ac.uk>
2023-04-07 22:36:31 +0100

    Readme clarification and hls script fix

--- README.md
+++ README.md
@@ -1,11 +1,111 @@
+[![DOI](https://zenodo.org/badge/606085080.svg)](https://zenodo.org/badge/latestdoi/606085080)
+
 # ATHEENA FCCM Artifacts
+
 Software and hardware artifacts for FCCM submission 2023
 
 There are three main artifacts:
 - Optimiser
-- HLS backend
 - Buffer Hardware Component (and generation)
+- HLS-based hardware generation using Vivado
 
-To generate an optimised FPGA accelerator descript for an Early-Exit network, follow the instructions in `optimiser/README.md`.
+## Full Setup instructions
 
-...
+The optimiser (and HLS generation) has been verified using the following software:
+
+- `conda=4.9.2, 4.10.1`
+- `python=3.7`
+
+### Optimiser package and Environment setup
+
+To install this package, run from this directory the following:
+
+```
+sudo apt install protobuf-compiler libprotoc-dev
+cd ./optimiser/
+conda env create -f atheena_opt_hls_p37.yml
+conda activate atheena_opt_hls_p37
+python atheena_setup.py install 
+```
+
+### Scala and Chisel package setup
+
+To install the appropriate software for buffer generation:
+
+> **Note** This module has been verified working for Ubuntu 20.04.6 LTS, Java version 11.0.18, sbt version 1.4.9, [Scala 2.12.13](https://www.scala-lang.org/download/2.12.13.html)
+
+The following instructions are taken from [Chisel's](https://github.com/chipsalliance/chisel3) instructions on environment setup:
+
+1. Install [Coursier](https://docs.scala-lang.org/getting-started/index.html#using-the-scala-installer-recommended-way) and follow the instructions.
+
+```
+curl -fL https://github.com/coursier/coursier/releases/latest/download/cs-x86_64-pc-linux.gz | gzip -d > cs && chmod +x cs 
+
+./cs setup
+```
+
+> **Note**: This will install the most recent version of scala. To check it has worked, run `scala -version` (a restart of the terminal maybe required).
+
+2. Install Scala version 2.12.13 and sbt version 1.4.9
+
+```
+cs install scala:2.12.13 && cs install scalac:2.12.13
+
+cs install sbt:1.4.9 && cs install sbtn:1.4.9
+```
+> **Note**: To check the scala and sbt versions, run `scala -version` and `sbt --script-version`.
+
+3. Regenerate the project for the buffer package.
+
+```
+cd ./buffer/
+
+sbt pack
+```
+
+### Vivado setup
+
+To install Vivado 2019.1:
+
+1. First [download](https://www.xilinx.com/support/download/index.html/content/xilinx/en/downloadNav/vivado-design-tools/2019-1.html) from the Xilinx website.
+
+2. Install the y2k22 patch according to these [instructions](https://support.xilinx.com/s/article/76960?language=en_US).
+
+3. Add the following to your ~/.bashrc file:
+
+```
+export PATH=/tools/Xilinx/Vivado/2019.1/bin:$PATH
+export PATH=/tools/Xilinx/SDK/2019.1/bin:$PATH
+export FPGACONVNET_ROOT=(path to repo)/ATHEENA_fccm_artifacts/hls
+export FPGACONVNET_HLS=(path to repo)/ATHEENA_fccm_artifacts/hls
+export FPGACONVNET_OPTIMISER=(path to repo)/ATHEENA_fccm_artifacts/optimiser
+```
+
+4. Once installed, you will also need to add a license server to your .bashrc file.
+
+5. You will need to setup JTAG drivers to program a device. To do so, execute the following script:
+
+```
+/tools/Xilinx/Vivado/2019.1/data/xicom/cable_drivers/lin64/install_script/install_drivers/install_drivers
+```
+
+For more information, visit [here](https://www.xilinx.com/support/answers/59128.html).
+
+Finally, there is a known [bug](http://svn.clifford.at/handicraft/2017/vivadobugs/vivadobug04.txt) to do with C++ libraries. A workaround for this is adding the `mpfr.h` and `gmp.h` headers manually. For this project, you need to create a header file `include/system.hpp` which includes the following:
+
+```C
+#ifndef SYSTEM_HPP_
+#define SYSTEM_HPP_
+
+#include "(path to Vivado 2019.1)/include/gmp.h"
+#include "(path to Vivado 2019.1)/include/mpfr.h"
+
+#endif
+```
+
+## Generating the Artifacts
+
+To generate an optimised FPGA accelerator description for an Early-Exit network, follow the instructions in `optimiser/README.md`.
+
+To generate the hardware corresponding to the accelerator description, copy the resulting `.json` file into `./hls/test/partitions/(new_folder)` and follow the HLS instructions.
+
---
+++ hls/scripts/split_run.sh
@@ -0,0 +1,212 @@
+#!/bin/bash
+
+# parsing some arguments - want to control generation
+ALL=0
+LYR_ARR=()
+PRT_IDX_ARG=0
+GEN_TOP=0
+RUN_VIVADO=0
+GEN_CODE=0
+
+NETWORK=brn_se_test
+MODEL_PATH=$FPGACONVNET_OPTIMISER/examples/models/atheena/branchy_lenet_20220902.onnx
+WEIGHTS_PATH=""
+
+while getopts "al:tvcn:m:h" opt; do
+    case ${opt} in
+        a ) ALL=1
+            GEN_TOP=1
+            #RUN_VIVADO=1
+            GEN_CODE=1
+            ;;
+        l ) set -f # disable glob
+            IFS=' ' # split on space char
+            LYR_ARR=($OPTARG)
+            PRT_IDX_ARG=${LYR_ARR[0]} # idx0 is partition
+            unset LYR_ARR[0]
+            ;;
+        t ) GEN_TOP=1;;
+        v ) RUN_VIVADO=1
+            echo "Running Vivado IP integrator"
+            ;;
+        c ) GEN_CODE=1
+            echo "Running host code generation"
+            ;;
+        n ) NETWORK=$OPTARG
+            ;;
+        m ) MODEL_PATH=$OPTARG
+            ;;
+        h )
+            echo "TODO Some useful help thing... [-a (gen all layers+top+host code)]"
+            echo "[-n (network+folder name) ]"
+            echo "[-m (onnx model path) ]"
+            echo
+            echo "[-l "arg1 arg2 arg3..."(partition:arg1, gen specific layer:arg2,3...) ]"
+            echo "[-t (generate top)] [-v (run vivado integrator)]"
+            echo "[-c (generate host code)]"
+            exit
+            ;;
+    esac
+done
+shift $((OPTIND -1))
+
+####################### Initialise variables ##########################
+PARTITION_INFO_PATH=*.json
+IMAGE_PATH=$FPGACONVNET_HLS/test/data/IMAGES_ee-pc70_bs1024
+ZYNQ_PART=xc7z045ffg900-2
+ZYNQ_BOARD=xilinx.com:zc706:part0:1.4
+#######################################################################
+
+# move into network folder
+cd $NETWORK
+
+FREQ=125
+# WARNING!!! might need to change to 64 so we have similar DMA overhead latency
+PORT_WIDTH=32 #NOTE dma port width different to axis port width
+WEIGHTS_RELOADING_FLAG=0 #NOTE not currently implemented
+SPLIT_NETWORK_FLAG=1 #extra arg for tcl scripts
+printf "Network name: ${NETWORK}\n"
+printf "Target operating frequency: ${FREQ}MHz\n"
+
+if [ $ALL -eq 1 ]
+then
+    # generate all the layers and top
+    echo "GENERATING ALL LAYERS IN HLS + TOP BLOCK"
+    # add all partitions and layers to go through
+    # get the number of partitions
+    NUM_PARTITIONS=$( jq '.partition | length' $PARTITION_INFO_PATH )
+    p_seq=$(seq 0 $((${NUM_PARTITIONS}-1)) )
+else
+    # do chosen layers in chosen partition
+    p_seq=$(seq ${PRT_IDX_ARG} ${PRT_IDX_ARG})
+    #echo "In partition ${PRT_IDX_ARG}, generating layers: ${LYR_ARR[@]}"
+fi
+
+# create outputs folder
+#mkdir -p outputs
+
+for i in $p_seq; do
+
+    # get current partition index
+    PARTITION_INDEX=$(($i))  #$(( $i - 1 ))
+
+    # create folders
+    mkdir -p partition_${PARTITION_INDEX}
+    #mkdir -p partition_${PARTITION_INDEX}/${NETWORK}_top/tb #FIXME add testing back in
+    mkdir -p partition_${PARTITION_INDEX}/${NETWORK}_top/src
+    mkdir -p partition_${PARTITION_INDEX}/${NETWORK}_top/include
+    mkdir -p partition_${PARTITION_INDEX}/data
+
+    if [ $ALL -eq 1 ]
+    then
+        #get number of layers
+        NUM_LYRS=$( jq --argjson pi $PARTITION_INDEX '.partition[$pi].layers | length' $PARTITION_INFO_PATH )
+        echo "number of layers $NUM_LYRS "
+        l_seq=$( seq 0 $((${NUM_LYRS}-1)) )
+    else
+        #use layer array
+        l_seq=$( seq 1 $((${#LYR_ARR[@]}+0)) )
+    fi
+
+    # create folders for each layer
+    if [ $ALL -eq 1 ] || [ $((${#LYR_ARR[@]}+0)) -gt 0 ]
+    then
+        for j in $l_seq; do
+            # get current layer index
+            LYR_INDEX=$(($j + 0)) # $(( $j - 1 ))
+            if [ $ALL -eq 1 ]
+            then
+                LYR_NAME=$( jq  -r --argjson pi $PARTITION_INDEX --argjson li $LYR_INDEX '.partition[$pi].layers[$li] | .name' $PARTITION_INFO_PATH)
+            else
+                LYR_NAME=${LYR_ARR[ $LYR_INDEX ]}
+            fi
+            #get name of each layer and create folder for compilation
+            echo "layer name: $LYR_NAME " #DEBUG
+
+            # create folders
+            mkdir -p partition_${PARTITION_INDEX}/${LYR_NAME}
+            mkdir -p partition_${PARTITION_INDEX}/${LYR_NAME}/src
+            mkdir -p partition_${PARTITION_INDEX}/${LYR_NAME}/include
+            #mkdir -p partition_${PARTITION_INDEX}/${LYR_NAME}/tb #FIXME add testing back in
+        done
+    fi
+
+    # create hardware, #-sp is split layer compilation
+    python $FPGACONVNET_HLS/scripts/generate_hardware.py -n $NETWORK -m $MODEL_PATH -p $PARTITION_INFO_PATH -i $PARTITION_INDEX -sp
+    # format weights
+    python $FPGACONVNET_HLS/scripts/format_weights.py -m $MODEL_PATH -p $PARTITION_INFO_PATH -i $PARTITION_INDEX
+    # format featuremaps
+    python $FPGACONVNET_HLS/scripts/format_featuremaps.py -m $MODEL_PATH -p $PARTITION_INFO_PATH -d $IMAGE_PATH -i $PARTITION_INDEX -s
+
+    if [ $GEN_CODE -eq 1 ]
+    then
+        # auto generate code - currently per partition
+        # NOTE not sure if network name is always used in code defs
+        python $FPGACONVNET_HLS/tools/split_hw_helper.py -p $PARTITION_INFO_PATH -pi $PARTITION_INDEX gen_host_code -n ${NETWORK}
+    fi
+
+    cd partition_${PARTITION_INDEX}
+
+    if [ $ALL -eq 1 ] || [ $((${#LYR_ARR[@]}+0)) -gt 0 ]
+    then
+        #TODO currently gen hw py script requires paths to exits, get rid of this so that only loop is hls gen
+        #synthesise implement and export ip for each layer - checking not performed
+        for k in $l_seq; do #does all layers
+            # get current layer index
+            LYR_INDEX=$(($k))  #$(( $i - 1 ))
+            if [ $ALL -eq 1 ]
+            then
+                LYR_NAME=$( jq  -r --argjson pi $PARTITION_INDEX --argjson li $LYR_INDEX '.partition[$pi].layers[$li] | .name' ../${PARTITION_INFO_PATH} )
+            else
+                LYR_NAME=${LYR_ARR[$LYR_INDEX]}
+            fi
+
+            #run vivado hls
+            cd ${LYR_NAME}
+            vivado_hls -f $FPGACONVNET_HLS/scripts/run_hls.tcl "_ -name ${LYR_NAME} -fpga ${ZYNQ_PART} -split_net_flag -fast -reset -type impl"
+            cd ..
+        done
+    fi
+
+    ##FIXME can't get include flags to work so copying files
+    mkdir -p tmp_inc
+
+    # top level axiu (stream) to stream with less signals
+    if [ $GEN_TOP -eq 1 ]
+    then
+        ##FIXME can't get include flags to work so copying files
+        cp */include/* ./tmp_inc/
+
+        cd ${NETWORK}_top
+        vivado_hls -f $FPGACONVNET_HLS/scripts/run_hls.tcl "_ -name ${NETWORK} -fpga ${ZYNQ_PART} -split_net_flag -fast -reset -type impl"
+        cd ..
+    fi
+
+    if [ $RUN_VIVADO -eq 1 ]
+    then
+        ################################# set up vivado hardware ################################
+        printf "\n\
+#################################\n\
+# Running tcl scripts in Vivado #\n\
+#################################\n"
+
+        #  ( argv 0 : network name          )
+        #  ( argv 1 : fpga part name        )
+        #  ( argv 2 : fpga board part       )
+        #  ( argv 3 : board frequency       )
+        #  ( argv 4 : port width            )
+        #  ( argv 5 : weights reloading on  )
+        #SPLIT NETWORK
+        #  ( argv 6 : split network on      )
+
+        vivado -mode batch -notrace -source $FPGACONVNET_HLS/scripts/gen_hw.tcl \
+            -tclargs    $NETWORK \
+                        $ZYNQ_PART \
+                        $ZYNQ_BOARD \
+                        $FREQ \
+                        $PORT_WIDTH \
+                        $WEIGHTS_RELOADING_FLAG \
+                        $SPLIT_NETWORK_FLAG
+    fi
+
+done
