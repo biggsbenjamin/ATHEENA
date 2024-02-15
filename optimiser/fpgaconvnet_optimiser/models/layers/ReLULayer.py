cb299a1489d44f3b16eb6dc88664d52cd6e9de87
biggsbenjamin <bb2515@ic.ac.uk>
2024-02-15 21:28:28 +0000

    Including relu rsc estimation

--- optimiser/fpgaconvnet_optimiser/models/layers/ReLULayer.py
+++ optimiser/fpgaconvnet_optimiser/models/layers/ReLULayer.py
@@ -70,6 +70,17 @@ class ReLULayer(Layer):
         self.modules['relu'].cols     = self.cols_in()
         self.modules['relu'].channels = int(self.channels_in()/self.coarse)
 
+    def resource(self):
+        relu_rsc    = self.modules['relu'].rsc()
+
+        # Total
+        return {
+            "LUT"  :  relu_rsc['LUT']*self.coarse,
+            "FF"   :  relu_rsc['FF']*self.coarse,
+            "BRAM" :  relu_rsc['BRAM']*self.coarse,
+            "DSP" :   relu_rsc['DSP']*self.coarse
+        }
+
     def visualise(self,name):
         cluster = pydot.Cluster(name,label=name)
 
